1960ef6150272c262ec4be878a2c6ed964557a19
KAZUKI27854 <02kazuki16@gmail.com>
2021-04-02 06:19:01 +0000

    create diary page

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -4,19 +4,34 @@ class DocumentsController < ApplicationController
 	end
 
 	def create
-		@document = Document.new(document_params)
+		document = Document.new(document_params)
+		document.user_id = current_user.id
+		document.save
+		redirect_to user_path(current_user.id)
 	end
 
 	def show
+		@document = Document.find(params[:id])
 	end
 
 	def edit
+		@document = Document.find(params[:id])
 	end
 
 	def update
+		document = Document.find(params[:id])
+		document.update(document_params)
+		redirect_to user_path(current_user.id)
 	end
 
 	def destroy
+		document = Document.find(params[:id])
+		document.destroy(document_params)
+		redirect_to user_path(current_user.id)
 	end
 
+	private
+	def document_params
+		params.require(:document).permit(:title, :body, :diary_image)
+	end
 end
