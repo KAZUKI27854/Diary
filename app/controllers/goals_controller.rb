9dc7b402c479cfcc398943a12acd07ebf999cec6
KAZUKI27854 <02kazuki16@gmail.com>
2021-04-07 15:06:38 +0000

    create valid for goal

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,20 @@
 class GoalsController < ApplicationController
+	def new
+		@goal = Goal.new
+	end
+
+	def create
+		@goal = Goal.new(goal_params)
+		@goal.user_id = current_user.id
+		if @goal.save
+		  redirect_to user_path(current_user.id)
+		else
+			render "new"
+		end
+	end
+
+	private
+	def goal_params
+		params.require(:goal).permit(:category, :goal_status, :milestone, :deadline)
+	end
 end
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,5 +1,14 @@
 class Goal < ApplicationRecord
+  MAX_GOALS_COUNT = 3
 
 	belongs_to :user
+
+	validate :goals_count_must_be_within_limit, on: :create
+
+	  def goals_count_must_be_within_limit
+	    if user.goals.count > MAX_GOALS_COUNT
+	      errors[:base] << "もくひょうは３つまでせっていできます"
+	    end
+	  end
 
 end
