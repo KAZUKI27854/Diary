61263e9a6e981cd9b0b11c54592fb1e0b3d04846
KAZUKI27854 <02kazuki16@gmail.com>
2021-04-07 13:10:37 +0000

    modified layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
+  	user_path(current_user.id)
+  end
+
+  def before_sign_in_path_for(resource)
   	root_path
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :documents, dependent: :destroy
-  has_many :goals
+  has_many :goals, dependent: :destroy
 
   attachment :profile_image
 end
