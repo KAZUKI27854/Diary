de68130bd8a6c495a8be92d4565f8f10b7921af2
KAZUKI27854 <02kazuki16@gmail.com>
2021-04-08 05:59:30 +0000

    rename diary_image

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_08_055203) do
+ActiveRecord::Schema.define(version: 2021_04_08_055514) do
 
   create_table "documents", force: :cascade do |t|
     t.text "title"
     t.text "body"
-    t.string "diary_image_id"
+    t.string "document_image_id"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
