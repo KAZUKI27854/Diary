5cda1dab2d5b3731f9e49509934aa0a8b65898b6
KAZUKI27854 <02kazuki16@gmail.com>
2021-04-09 00:10:13 +0000

    create about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  get 'about' => 'homes#about'
   resources :users, only: [:show, :edit, :update, :destroy]
   resources :documents, except: [:index]
   resources :goals, except: [:index]
