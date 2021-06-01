e79272fa91cbe842b0e4a3f1a024094d1abaa76a
KAZUKI27854 <02kazuki16@gmail.com>
2021-06-01 06:38:35 +0000

    [add]is_active_column to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_30_063828) do
+ActiveRecord::Schema.define(version: 2021_06_01_063303) do
 
   create_table "documents", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2021_05_30_063828) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "profile_image_id"
+    t.boolean "is_active", default: true, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
