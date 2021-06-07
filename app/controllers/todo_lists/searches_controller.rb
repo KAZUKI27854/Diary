7c9e55e647100f01b57853c7aa023dbb0f8a6931
KAZUKI27854 <02kazuki16@gmail.com>
2021-06-07 08:46:27 +0000

    [add]set_current_user to searches_controller

--- app/controllers/todo_lists/searches_controller.rb
+++ app/controllers/todo_lists/searches_controller.rb
@@ -1,15 +1,22 @@
 class TodoLists::SearchesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_current_user
 
   def index
-    @todo_lists = current_user.todo_lists.where('body LIKE(?)', "%#{params[:word]}%").page(params[:page])
+    @todo_lists = @user.todo_lists.where('body LIKE(?)', "%#{params[:word]}%").page(params[:page])
 
     if params[:word].blank?
-      todo_lists = current_user.todo_lists.classify.page(params[:page])
+      todo_lists = @user.todo_lists.classify.page(params[:page])
 			render partial: "todo_lists/todo_list", collection: todo_lists
     else
       render partial: "todo_lists/todo_list", collection: @todo_lists
     end
 
   end
+
+  private
+
+    def set_current_user
+      @user = current_user
+    end
 end
