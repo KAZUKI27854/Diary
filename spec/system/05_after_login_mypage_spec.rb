4e4c2dd3bb89a75604c89a8712e1fb56d2c265cb
KAZUKI27854 <02kazuki16@gmail.com>
2021-06-13 03:26:11 +0000

    [add]test for user-edit-modal

--- spec/system/05_after_login_mypage_spec.rb
+++ spec/system/05_after_login_mypage_spec.rb
@@ -1,11 +1,63 @@
 require 'rails_helper'
 
-describe '5.ユーザログイン後のマイページのテスト', type: :feature do
+describe '5.ユーザログイン後のマイページのテスト', type: :feature, js: true do
   let!(:user) { create(:user) }
 
   before do
     login_as(user, :scope => :user)
     visit  my_page_path
+  end
+
+  describe '非表示確認テスト' do
+    it '全てのモーダルウインドウが非表示になっている' do
+      expect(page).not_to have_selector('.modal')
+    end
+    it 'ユーザーと目標のメニューが非表示になっている' do
+      expect(page).not_to have_selector('.my-page__menu')
+    end
+  end
+
+  describe 'ユーザーメニューのテスト' do
+    before do
+      all('.my-page__menu--icon')[0].click
+    end
+
+    it 'ユーザーアイコンをクリックするとユーザーメニューが表示される' do
+      expect(page).to have_selector('.my-page__menu')
+      expect(page).to have_content 'なまえ'
+    end
+    context 'ユーザー編集モーダルのテスト' do
+      before do
+        click_on 'へんしゅう'
+      end
+
+      it '「へんしゅう」をクリックするとユーザー編集のモーダルが表示される' do
+        expect(page).to have_selector('#modal-user-edit')
+      end
+      it '名前のフォームが表示される' do
+        expect(page).to have_field 'user[name]'
+      end
+      it 'プロフィール画像のフォームが表示される' do
+        expect(page).to have_field 'user[profile_image]'
+      end
+      it '名前とプロフィール画像の変更が成功する' do
+        fill_in 'user[name]', with: 'テストユーザー'
+        attach_file 'user[profile_image]', "#{Rails.root}/app/assets/images/character/brave.png"
+        expect{ click_button 'へんこう' }.to change{ User.first.name }.to('テストユーザー')
+        expect([User.first.profile_image_id]).to be_present
+      end
+      it 'エラーメッセージが正しく表示される' do
+        fill_in 'user[name]', with: ''
+        click_button 'へんこう'
+        expect(page).to have_selector '.error__message'
+      end
+    end
+    context '退会確認モーダルのテスト' do
+      it '「退会する」をクリックすると退会確認画面へ遷移する' do
+        click_on '退会する'
+        expect(page).to have_selector '.withdraw-confirm'
+      end
+    end
   end
 
   describe '目標データ作成前のテスト' do
@@ -14,13 +66,17 @@ describe '5.ユーザログイン後のマイページのテスト', type: :feat
       expect(goal_icon['class']).to include 'js-bound'
     end
 
-    it 'ドキュメント作成アイコンがクリックできないようになっている', js: true do
-      # find('.js-save-link').click
+    xit 'ドキュメント作成モーダルへのリンクが無効化されている', js: true do
+      #find('.js-save-link').click
+      expect( find('.js-save-link').click ).to raise_error
       #expect(find('#modal-new-doc', visible: false)).to_not be_visible
-      #expect(page).to have_selector '.new-doc'
+      #expect(page).not_to have_selector '.new-doc'
       #expect(page).to have_no_content 'ぼうけんをきろくできるぞ'
       # binding.irb
-      expect(page).not_to have_selector('.modal')
+    end
+
+    it 'サイドメニューの目標アイコンから目標設定のモーダルが表示できる', js: true do
+
     end
   end
 end
