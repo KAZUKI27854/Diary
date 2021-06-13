f9012353249c3d19418b0efc12873538a550f566
KAZUKI27854 <02kazuki16@gmail.com>
2021-06-13 11:55:10 +0000

    [refactor]test_06 in rspec

--- spec/system/06_after_login_goal_spec.rb
+++ spec/system/06_after_login_goal_spec.rb
@@ -33,15 +33,19 @@ describe '6.ユーザログイン後の目標関連のテスト', type: :feature
       it '目標メニューの「せっていする」をクリックすると目標作成モーダルが表示される' do
         expect(page).to have_selector '#modal-new-goal'
       end
+
       it 'カテゴリー入力フォームが表示される' do
         expect(page).to have_field 'goal[category]'
       end
+
       it '最終目標入力フォームが表示される' do
         expect(page).to have_field 'goal[goal_status]'
       end
+
       it '目標期限入力フォームが表示される' do
         expect(page).to have_field 'goal[deadline]'
       end
+
       it '目標作成成功のテスト' do
         fill_in 'goal[category]', with: Faker::Games::Pokemon.move
         fill_in 'goal[goal_status]', with: Faker::Games::Pokemon.move
@@ -49,6 +53,7 @@ describe '6.ユーザログイン後の目標関連のテスト', type: :feature
         expect{ click_on 'せってい' }.to change{ Goal.count }.by(1)
         expect(page).to have_content 'もくひょうをついかしました'
       end
+
       it '目標作成失敗のテスト' do
         fill_in 'goal[category]', with: ''
         fill_in 'goal[goal_status]', with: ''
@@ -73,16 +78,20 @@ describe '6.ユーザログイン後の目標関連のテスト', type: :feature
       goal_icon = find '.my-page__menu--icon--goal'
       expect(goal_icon['class']).not_to include 'js-bound'
     end
+
     it '目標メニューに設定した目標のカテゴリーが表示されている' do
       expect(page).to have_content goal.category
     end
+
     it '設定した目標の、次の目標が「ぼうけんをきろくする」になっている' do
       expect(page).to have_content 'ぼうけんをきろくする'
     end
+
     it '「ぼうけんをきろくする」をクリックすると、ドキュメント作成モーダルが表示される' do
       click_on 'ぼうけんをきろくする'
       expect(page).to have_selector '#modal-new-doc'
     end
+
     context '目標編集モーダルのテスト' do
       before do
         click_on goal.category
@@ -91,6 +100,7 @@ describe '6.ユーザログイン後の目標関連のテスト', type: :feature
       it '目標のカテゴリーをクリックするとその目標の編集モーダルが表示される' do
         expect(page).to have_selector '#modal-goal' + goal.id.to_s + '-edit'
       end
+
       it '目標編集成功のテスト' do
         fill_in 'goal[category]', with: 'テスト'
         fill_in 'goal[goal_status]', with: 'テスト成功'
@@ -99,12 +109,14 @@ describe '6.ユーザログイン後の目標関連のテスト', type: :feature
         expect(goal.reload.category).to eq 'テスト'
         expect(goal.reload.goal_status).to eq 'テスト成功'
       end
+
       it '目標編集失敗のテスト' do
         fill_in 'goal[category]', with: ''
         fill_in 'goal[goal_status]', with: ''
         click_on 'へんこう'
         expect(page).to have_selector '.error__message'
       end
+
       it '目標削除のテスト' do
         page.accept_confirm do
           click_on 'さくじょ'
