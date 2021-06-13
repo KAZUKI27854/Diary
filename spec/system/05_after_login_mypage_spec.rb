390fbf6b8fb0dab486d3aa73fab443820c1060bc
KAZUKI27854 <02kazuki16@gmail.com>
2021-06-13 07:35:52 +0000

    [add]test for goal_menu

--- spec/system/05_after_login_mypage_spec.rb
+++ spec/system/05_after_login_mypage_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe '5.ユーザログイン後のマイページのテスト', type: :feature, js: true do
+describe '5.ユーザログイン後のメニュー画面のテスト', type: :feature, js: true do
   let!(:user) { create(:user) }
 
   before do
@@ -91,23 +91,110 @@ describe '5.ユーザログイン後のマイページのテスト', type: :feat
     end
   end
 
-  describe '目標データ作成前のテスト' do
-    it 'メニューの目標アイコンにバウンドアニメーションの為のクラスが追加されている', js: true do
+  describe '目標作成前の目標メニューのテスト' do
+    let!(:stage) { create(:stage) }
+
+    before do
+      all('.my-page__menu--icon')[1].click
+    end
+
+    it '目標アイコンにバウンドアニメーションの為のクラスが追加されている' do
       goal_icon = find '.my-page__menu--icon--goal'
       expect(goal_icon['class']).to include 'js-bound'
     end
 
-    xit 'ドキュメント作成モーダルへのリンクが無効化されている', js: true do
-      #find('.js-save-link').click
-      expect( find('.js-save-link').click ).to raise_error
-      #expect(find('#modal-new-doc', visible: false)).to_not be_visible
-      #expect(page).not_to have_selector '.new-doc'
-      #expect(page).to have_no_content 'ぼうけんをきろくできるぞ'
-      # binding.irb
+    it '目標アイコンをクリックすると目標メニューが表示される' do
+      expect(page).to have_selector '.my-page__menu'
+      expect(page).to have_content 'せっていする'
     end
 
-    it 'サイドメニューの目標アイコンから目標設定のモーダルが表示できる', js: true do
+    context '目標作成モーダルのテスト' do
+      before do
+        click_on 'せっていする'
+      end
 
+      it '目標メニューの「せっていする」をクリックすると目標作成モーダルが表示される' do
+        expect(page).to have_selector '#modal-new-goal'
+      end
+      it 'カテゴリー入力フォームが表示される' do
+        expect(page).to have_field 'goal[category]'
+      end
+      it '最終目標入力フォームが表示される' do
+        expect(page).to have_field 'goal[goal_status]'
+      end
+      it '目標期限入力フォームが表示される' do
+        expect(page).to have_field 'goal[deadline]'
+      end
+      it '目標作成成功のテスト' do
+        fill_in 'goal[category]', with: Faker::Games::Pokemon.move
+        fill_in 'goal[goal_status]', with: Faker::Games::Pokemon.move
+        fill_in 'goal[deadline]', with: Faker::Date.in_date_period
+        expect{ click_on 'せってい' }.to change{ Goal.count }.by(1)
+        expect(page).to have_content 'もくひょうをついかしました'
+      end
+      it '目標作成失敗のテスト' do
+        fill_in 'goal[category]', with: ''
+        fill_in 'goal[goal_status]', with: ''
+        fill_in 'goal[deadline]', with: ''
+        expect{ click_on 'せってい' }.to change{ Goal.count }.by(0)
+        expect(page).to have_selector '.error__message'
+      end
+    end
+  end
+
+  describe '目標作成後の目標メニューのテスト' do
+    let!(:stage) { create(:stage) }
+    let!(:goal) { create(:goal, user_id: user.id) }
+
+    before do
+      #通常であれば目標作成後にマイページへリダイレクトされる為
+      visit my_page_path
+      all('.my-page__menu--icon')[1].click
+    end
+
+    it '目標アイコンにバウンドアニメーションの為のクラスがない' do
+      goal_icon = find '.my-page__menu--icon--goal'
+      expect(goal_icon['class']).not_to include 'js-bound'
+    end
+    it '目標メニューに設定した目標のカテゴリーが表示されている' do
+      expect(page).to have_content goal.category
+    end
+    it '設定した目標の、次の目標が「ぼうけんをきろくする」になっている' do
+      expect(page).to have_content 'ぼうけんをきろくする'
+    end
+    it '「ぼうけんをきろくする」をクリックすると、ドキュメント作成モーダルが表示される' do
+      click_on 'ぼうけんをきろくする'
+      expect(page).to have_selector '#modal-new-doc'
+    end
+    context '目標編集モーダルのテスト' do
+      before do
+        click_on goal.category
+      end
+
+      it '目標のカテゴリーをクリックするとその目標の編集モーダルが表示される' do
+        expect(page).to have_selector '#modal-goal' + goal.id.to_s + '-edit'
+      end
+      it '目標編集成功のテスト' do
+        fill_in 'goal[category]', with: 'テスト'
+        fill_in 'goal[goal_status]', with: 'テスト成功'
+        click_on 'へんこう'
+        expect(page).to have_content 'もくひょうをへんこうしました'
+        expect(goal.reload.category).to eq 'テスト'
+        expect(goal.reload.goal_status).to eq 'テスト成功'
+      end
+      it '目標編集失敗のテスト' do
+        fill_in 'goal[category]', with: ''
+        fill_in 'goal[goal_status]', with: ''
+        click_on 'へんこう'
+        expect(page).to have_selector '.error__message'
+      end
+      it '目標削除のテスト' do
+        page.accept_confirm do
+          click_on 'さくじょ'
+        end
+        expect(page).to have_content 'もくひょうをさくじょしました'
+        expect(Goal.count).to eq 0
+      end
     end
   end
 end
