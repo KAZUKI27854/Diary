44158194b8995f35ba2ec0e2b00887d83bc5da87
KAZUKI27854 <02kazuki16@gmail.com>
2021-06-15 11:21:03 +0000

    [add]model_test for document

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,47 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe 'Documentモデルのテスト', type: :model do
-  describe 'バリデーションのテスト' do
+  it '全ての項目を入力すればデータ保存ができる' do
+    expect(build(:document)).to be_valid
+  end
+
+  describe 'カラムごとのバリデーションとエラーメッセージのテスト' do
     subject { document.valid? }
 
-    let(:user) { create(:user) }
-    let(:goal) { create(:goal, user_id: user.id) }
-    let(:document) { build(:document, user_id: user.id, goal_id: goal.id) }
+    let(:document) { build(:document) }
 
     context 'bodyカラム' do
-      it '空欄でないこと' do
+      it '空欄の場合保存に失敗し、空白に関するエラーメッセージが返される' do
         document.body = ''
+
         is_expected.to eq false
+        expect(document.errors[:body]).to include("を入力してください")
       end
-      it '100文字以内であること: 101文字は×' do
-        document.body = Faker::Lorem.characters(number: 101)
+
+      it '300文字以内であること: 301文字は保存に失敗し、文字数超過のエラーメッセージが返される' do
+        document.body = Faker::Lorem.characters(number: 301)
+
         is_expected.to eq false
+        expect(document.errors[:body]).to include("は300文字以内で入力してください")
       end
-      it '100文字以内であること: 100文字は〇' do
-        document.body = Faker::Lorem.characters(number: 100)
+
+      it '300文字以内であること: 300文字は保存に成功する' do
+        document.body = Faker::Lorem.characters(number: 300)
         is_expected.to eq true
       end
     end
 
     context 'milestoneカラム' do
-      it '空欄でないこと' do
+      it '空欄の場合保存に失敗し、空白に関するエラーメッセージが返される' do
         document.milestone = ''
+
         is_expected.to eq false
+        expect(document.errors[:milestone]).to include("を入力してください")
       end
-      it '100文字以内であること: 101文字は×' do
+
+      it '100文字以内であること: 101文字は保存に失敗し、文字数超過のエラーメッセージが返される' do
         document.milestone = Faker::Lorem.characters(number: 101)
+
         is_expected.to eq false
+        expect(document.errors[:milestone]).to include("は100文字以内で入力してください")
       end
-      it '100文字以内であること: 100文字は〇' do
+
+      it '100文字以内であること: 100文字は保存に成功する' do
         document.milestone = Faker::Lorem.characters(number: 100)
         is_expected.to eq true
       end
     end
 
     context 'add_levelカラム' do
-      it '空欄でないこと' do
+      it '空欄の場合保存に失敗し、空白に関するエラーメッセージが返される' do
         document.add_level = ''
+
         is_expected.to eq false
+        expect(document.errors[:add_level]).to include("を入力してください")
       end
     end
   end
@@ -52,6 +68,7 @@ RSpec.describe 'Documentモデルのテスト', type: :model do
         expect(Document.reflect_on_association(:user).macro).to eq :belongs_to
       end
     end
+    
     context 'goalモデルとの関係' do
       it 'N:1となっている' do
         expect(Document.reflect_on_association(:goal).macro).to eq :belongs_to
