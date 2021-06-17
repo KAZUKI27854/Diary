0c4ab13b577672643affdc8c7ea4dbc9c9fdefdc
KAZUKI27854 <02kazuki16@gmail.com>
2021-06-17 11:53:06 +0000

    [add]config for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.default_url_options = { :host => 'dot-diary.online'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => "DotDiary.ambitionofmikkabozu@gmail.com",
+    :password => ENV['GMAIL_PASSWORD'],
+    :authentication => 'login',
+    :enable_starttls_auto => true
+  }
 end
