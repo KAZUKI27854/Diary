6a339bd1762886578f08203a8b1f393e4ab94f4c
KAZUKI27854 <02kazuki16@gmail.com>
2021-06-19 02:32:27 +0000

    [add]validation to todo_list_deadline

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -5,4 +5,9 @@ class TodoList < ApplicationRecord
   belongs_to :user
 
   validates :body, {presence: true, length: {maximum: 100}}
+  validate :day_after_today
+
+	def day_after_today
+		errors.add(:deadline, 'は、本日以降の日付を入力して下さい') if deadline.to_date < Date.today
+	end
 end
