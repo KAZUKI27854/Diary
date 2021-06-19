5f83df2cc2f401f3e9a5a3987349d0c327e5ec6e
KAZUKI27854 <02kazuki16@gmail.com>
2021-06-19 08:22:06 +0000

    [add]notice_deadline for goal

--- app/helpers/goals_helper.rb
+++ app/helpers/goals_helper.rb
@@ -3,4 +3,18 @@ module GoalsHelper
     documents = Document.where(goal_id: goal_id).order(updated_at: :desc)
     documents.first.milestone
   end
+
+  def near_deadline_goal_count
+    tomorrow = Date.tomorrow
+    after_3_days = tomorrow + 2
+    current_user.goals.where(:deadline => tomorrow..after_3_days).count
+  end
+
+  def today_deadline_goal_count
+    current_user.goals.where(deadline: Date.today.all_day).count
+  end
+
+  def over_deadline_goal_count
+    current_user.goals.where("deadline < ?", Date.yesterday).count
+  end
 end
