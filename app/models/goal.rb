98af5a3e871569933ae25b362dcb999c4eab3011
KAZUKI27854 <02kazuki16@gmail.com>
2021-06-19 02:27:10 +0000

    [add]validation to goal_deadline

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -7,4 +7,9 @@ class Goal < ApplicationRecord
 	validates :category, {presence: true, length: {maximum: 20}}
 	validates :goal_status, {presence: true, length: {maximum: 100}}
 	validates :deadline, presence: true
+	validate :day_after_tomorrow
+
+	def day_after_tomorrow
+		errors.add(:deadline, 'は、明日以降の日付を入力して下さい') if deadline.to_date <= Date.today
+	end
 end
