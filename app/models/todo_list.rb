8f11bb18d961eb1cf9cec9977e3e030e49d3e9df
KAZUKI27854 <02kazuki16@gmail.com>
2021-06-20 06:18:12 +0000

    [add]comment to todo_list_validation

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -7,7 +7,7 @@ class TodoList < ApplicationRecord
   validates :body, {presence: true, length: {maximum: 100}}
   validate :day_after_today
 
-  #過去の日付登録を防ぐバリデーション（期限超過の場合、チェックを入れることだけできる）
+  #過去の日付登録を防ぐバリデーション（期限超過の場合、期限変更かチェックを入れることだけできる）
   def day_after_today
     if deadline != nil && is_finished ==false
       errors.add(:deadline, 'は、本日以降の日付を入力して下さい') if deadline.to_date < Date.today
