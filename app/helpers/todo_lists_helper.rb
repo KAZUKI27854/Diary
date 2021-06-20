97c4c9df8d6e8df991f4ec544d88958b7a7bd436
KAZUKI27854 <02kazuki16@gmail.com>
2021-06-20 02:50:11 +0000

    [add]notice_deadline to TodoList

--- app/helpers/todo_lists_helper.rb
+++ app/helpers/todo_lists_helper.rb
@@ -1,15 +1,19 @@
 module TodoListsHelper
+  def not_finished_lists
+    current_user.todo_lists.where.not(priority: 2)
+  end
+
   def near_deadline_list_count
     tomorrow = Date.tomorrow
     after_3_days = tomorrow + 2
-    current_user.todo_lists.where(:deadline => tomorrow..after_3_days).count
+    not_finished_lists.where(:deadline => tomorrow..after_3_days).count
   end
 
   def today_deadline_list_count
-    current_user.todo_lists.where(deadline: Date.today.all_day).count
+    not_finished_lists.where(deadline: Date.today.all_day).count
   end
 
   def over_deadline_list_count
-    current_user.todo_lists.where("deadline < ?", Date.yesterday).count
+    not_finished_lists.where("deadline < ?", Date.yesterday).count
   end
 end
