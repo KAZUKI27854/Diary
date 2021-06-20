6e0c6e9105e289aeabf1822525ad4de528241f2d
KAZUKI27854 <02kazuki16@gmail.com>
2021-06-20 01:16:05 +0000

    [refactor]indential of goal.rb

--- app/helpers/todo_lists_helper.rb
+++ app/helpers/todo_lists_helper.rb
@@ -1,2 +1,15 @@
 module TodoListsHelper
+  def near_deadline_list_count
+    tomorrow = Date.tomorrow
+    after_3_days = tomorrow + 2
+    current_user.todo_lists.where(:deadline => tomorrow..after_3_days).count
+  end
+
+  def today_deadline_list_count
+    current_user.todo_lists.where(deadline: Date.today.all_day).count
+  end
+
+  def over_deadline_list_count
+    current_user.todo_lists.where("deadline < ?", Date.yesterday).count
+  end
 end
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,17 +1,17 @@
 class Goal < ApplicationRecord
-	belongs_to :user
-	belongs_to :stage
-	has_many :documents, dependent: :destroy
-	has_many :todo_lists, dependent: :destroy
+  belongs_to :user
+  belongs_to :stage
+  has_many :documents, dependent: :destroy
+  has_many :todo_lists, dependent: :destroy
+	
+  validates :category, {presence: true, length: {maximum: 20}}
+  validates :goal_status, {presence: true, length: {maximum: 100}}
+  validates :deadline, presence: true
+  validate :day_after_tomorrow
 
-	validates :category, {presence: true, length: {maximum: 20}}
-	validates :goal_status, {presence: true, length: {maximum: 100}}
-	validates :deadline, presence: true
-	validate :day_after_tomorrow
-
-	def day_after_tomorrow
-		unless deadline == nil
-		  errors.add(:deadline, 'は、明日以降の日付を入力して下さい') if deadline.to_date <= Date.today
-		end
-	end
+  def day_after_tomorrow
+    unless deadline == nil
+      errors.add(:deadline, 'は、明日以降の日付を入力して下さい') if deadline.to_date <= Date.today
+    end
+  end
 end
