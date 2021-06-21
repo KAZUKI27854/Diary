4162605ac9ea61f7f7e9d0a3665f6c27c9215e30
KAZUKI27854 <02kazuki16@gmail.com>
2021-06-21 06:59:43 +0000

    [refactor]rspec_test_08

--- spec/features/08_todo_list_spec.rb
+++ spec/features/08_todo_list_spec.rb
@@ -15,7 +15,7 @@ describe '8.ユーザログイン後のTodoリスト関連のテスト', type: :
   end
 
   context 'リンクのテスト' do
-    #ドキュメントが作成されていない状態ではTodoリストアイコンがクリックできない仕様の為
+    # ドキュメントが作成されていない状態ではTodoリストアイコンがクリックできない仕様の為
     let!(:document) { create(:document, user_id: user.id, goal_id: goal.id) }
 
     before do
@@ -79,7 +79,7 @@ describe '8.ユーザログイン後のTodoリスト関連のテスト', type: :
 
     it 'Todoリスト作成失敗のテスト' do
       fill_in 'todo_list[body]', with: ''
-      expect{ click_button 'ついか' }.to change{ TodoList.count }.by(0)
+      expect { click_button 'ついか' }.to change(TodoList, :count).by(0)
       expect(page).to have_selector '.error__message'
     end
 
@@ -165,19 +165,13 @@ describe '8.ユーザログイン後のTodoリスト関連のテスト', type: :
     end
   end
 
-  context 'Todoリスト一覧とチェックのテスト' do
+  context 'Todoリストチェックと一覧のテスト' do
     let!(:list) { create(:todo_list, user_id: user.id, goal_id: goal.id) }
     let!(:list_with_deadline) { create(:todo_list, :with_deadline, user_id: user.id, goal_id: goal.id) }
     let!(:list_finished) { create(:todo_list, :finished, user_id: user.id, goal_id: goal.id) }
 
     before do
       visit current_path
-    end
-
-    it 'Todoリストがpriorityカラムの数値順に並んでいる' do
-      expect(all('.js-todo-lists div')[0]).to have_content list_with_deadline.body
-      expect(all('.js-todo-lists div')[5]).to have_content list.body
-      expect(all('.js-todo-lists div')[10]).to have_content list_finished.body
     end
 
     it '終了していないリストのチェックアイコンにチェックが入っていない' do
@@ -194,7 +188,7 @@ describe '8.ユーザログイン後のTodoリスト関連のテスト', type: :
     end
 
     it '空のチェックボックスをクリックすると、priorityとis_finishedカラムのデータが更新される' do
-      #期限なしのリストにチェックを入れる
+      # 期限なしのリストにチェックを入れる
       all('.todo-lists__icon--check')[2].click
 
       expect(list.reload.priority).to eq 2
@@ -202,16 +196,16 @@ describe '8.ユーザログイン後のTodoリスト関連のテスト', type: :
     end
 
     it 'リストへチェックを入れると、表示順が入れ替わっている' do
-      #期限付きのリストにチェックを入れる
+      # 期限付きのリストにチェックを入れる
       all('.todo-lists__icon--check')[1].click
 
-      #唯一チェックの入っていないlistのデータが最上部に表示されている
+      # 唯一チェックの入っていないlistのデータが最上部に表示されている
       sleep 1
       expect(all('.js-todo-lists div')[0]).to have_content list.body
     end
 
     it '期限付きでチェック済みのリストのチェックを外すと、priorityとis_finishedカラムのデータが更新される' do
-      #期限付きのリストにチェックを入れる => 外す
+      # 期限付きのリストにチェックを入れる => 外す
       all('.todo-lists__icon--check')[1].click
       sleep 1
       all('.todo-lists__icon--check')[3].click
@@ -221,7 +215,7 @@ describe '8.ユーザログイン後のTodoリスト関連のテスト', type: :
     end
 
     it '期限なしでチェック済みのリストのチェックを外すと、priorityとis_finishedカラムのデータが更新される' do
-      #終了しているリストのチェックを外す
+      # 終了しているリストのチェックを外す
       all('.todo-lists__icon--check')[3].click
 
       expect(list_finished.reload.priority).to eq 1
@@ -229,7 +223,7 @@ describe '8.ユーザログイン後のTodoリスト関連のテスト', type: :
     end
 
     it '「チェックずみをさくじょ」ボタンをクリックすると、チェック済みリストが全て削除される' do
-      #全てのリストをチェック済みにする
+      # 全てのリストをチェック済みにする
       all('.todo-lists__icon--check')[2].click
       sleep 1
       all('.todo-lists__icon--check')[1].click
@@ -241,34 +235,31 @@ describe '8.ユーザログイン後のTodoリスト関連のテスト', type: :
       expect(page).to have_selector '.todo-lists'
       expect(TodoList.count).to eq 0
     end
+
+    it 'Todoリストがpriorityカラムの数値順に並んでいる' do
+      # 終了しているリストが3番目に表示されているかチェックボックスから確認
+      expect(all('.todo-lists__icon--check')[1]['alt']).to eq 'checkbox-icon'
+      expect(all('.todo-lists__icon--check')[2]['alt']).to eq 'checkbox-icon'
+      expect(all('.todo-lists__icon--check')[3]['alt']).to eq 'check-icon'
+
+      # 1,2番目の表示内容が正しいか確認
+      expect(all('.todo-lists__link--edit')[0]).to have_content list_with_deadline.body
+      expect(all('.todo-lists__link--edit')[1]).to have_content list.body
+    end
   end
 
   describe 'Todoリスト検索のテスト' do
-    let!(:goal_2) { create(:goal, user_id: user.id)}
+    let!(:goal_2) { create(:goal, user_id: user.id) }
 
-    #目標1に関するTodoリスト
+    # 目標1に関するTodoリスト
     let!(:list) { create(:todo_list, :list_1, user_id: user.id, goal_id: goal.id) }
     let!(:list_2) { create(:todo_list, :list_2, user_id: user.id, goal_id: goal.id) }
 
-    #目標2に関するTodoリスト
+    # 目標2に関するTodoリスト
     let!(:list_3) { create(:todo_list, :list_3, user_id: user.id, goal_id: goal_2.id) }
     let!(:list_4) { create(:todo_list, :list_4, user_id: user.id, goal_id: goal_2.id) }
 
     before do
-      def expect_all_lists_exist
-        expect(page).to have_content list.body
-        expect(page).to have_content list_2.body
-        expect(page).to have_content list_3.body
-        expect(page).to have_content list_4.body
-      end
-
-      def expect_only_first_list_exist
-        expect(page).to have_content list.body
-        expect(page).not_to have_content list_2.body
-        expect(page).not_to have_content list_3.body
-        expect(page).not_to have_content list_4.body
-      end
-
       def select_first_goal_category
         within '#goal_category' do
           select goal.category
@@ -303,7 +294,7 @@ describe '8.ユーザログイン後のTodoリスト関連のテスト', type: :
 
       it '検索用のセレクトボックスを選択後、「すべて」に戻すと全てのリストが表示される' do
         reset_select_box
-        expect_all_lists_exist
+        expect(page).to have_selector('.todo-lists__link--edit', count: 4)
       end
     end
 
@@ -314,12 +305,14 @@ describe '8.ユーザログイン後のTodoリスト関連のテスト', type: :
 
       it '検索用フォームに文字を入力すると、本文にその文字が含まれるリストのみ表示される' do
         sleep 1
-        expect_only_first_list_exist
+        # 1つ目という文字列が含まれたリスト1だけ表示されているか
+        expect(page).to have_selector('.todo-lists__link--edit', count: 1)
+        expect(page).to have_content list.body
       end
 
       it '入力フォームを空にすると全てのリストが表示される' do
         reset_text_field
-        expect_all_lists_exist
+        expect(page).to have_selector('.todo-lists__link--edit', count: 4)
       end
     end
 
@@ -330,13 +323,15 @@ describe '8.ユーザログイン後のTodoリスト関連のテスト', type: :
       end
 
       it '同時に検索ができる' do
-        expect_only_first_list_exist
+        # 目標1かつ1という文字列が含まれたリスト1だけ表示されているか
+        expect(page).to have_selector('.todo-lists__link--edit', count: 1)
+        expect(page).to have_content list.body
       end
 
       it 'セレクトボックスを「すべてのもくひょう」に戻すと、フォームの文字でのみ検索され、表示される' do
         reset_select_box
 
-        #1という文字列が含まれたリスト1,4だけ表示されているか
+        # 1という文字列が含まれたリスト1,4だけ表示されているか
         expect(page).to have_content list.body
         expect(page).to have_content list_4.body
 
@@ -347,7 +342,7 @@ describe '8.ユーザログイン後のTodoリスト関連のテスト', type: :
       it '入力フォームを空にすると、セレクトボックスの目標でのみ検索され、表示される' do
         reset_text_field
 
-        #目標1に関する投稿である、リスト1,2だけ表示されているか
+        # 目標1に関する投稿である、リスト1,2だけ表示されているか
         expect(page).to have_content list.body
         expect(page).to have_content list_2.body
 
@@ -359,7 +354,7 @@ describe '8.ユーザログイン後のTodoリスト関連のテスト', type: :
         reset_select_box
         reset_text_field
 
-        expect_all_lists_exist
+        expect(page).to have_selector('.todo-lists__link--edit', count: 4)
       end
     end
   end
