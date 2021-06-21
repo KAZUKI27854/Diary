0fb9cf14f81d93997b81eea08d0a1f911c929b73
KAZUKI27854 <02kazuki16@gmail.com>
2021-06-21 04:43:40 +0000

    [add]rubocop and refactor Documents/searches_controller.rb

--- app/controllers/documents/searches_controller.rb
+++ app/controllers/documents/searches_controller.rb
@@ -3,14 +3,14 @@ class Documents::SearchesController < ApplicationController
   before_action :set_current_user
 
   def index
-    if params[:category].blank? && params[:word].blank? then
+    if params[:category].blank? && params[:word].blank?
       documents = @user.documents
-    elsif params[:category].present? && params[:word].present? then
+    elsif params[:category].present? && params[:word].present?
 
-      doc_result_by_cat =  @user.goals.find_by(category: params[:category]).documents
+      doc_result_by_cat = @user.goals.find_by(category: params[:category]).documents
       documents = doc_result_by_cat.where('body LIKE(?)', "%#{params[:word]}%")
 
-    elsif params[:category].present? && params[:word].blank? then
+    elsif params[:category].present? && params[:word].blank?
       documents = @user.goals.find_by(category: params[:category]).documents
     else
       documents = @user.documents.where('body LIKE(?)', "%#{params[:word]}%")
@@ -22,7 +22,7 @@ class Documents::SearchesController < ApplicationController
 
   private
 
-    def set_current_user
-      @user = current_user
-    end
+  def set_current_user
+    @user = current_user
+  end
 end
