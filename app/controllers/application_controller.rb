040f78ab693c9dd896b07e216b03987b9c6720c6
KAZUKI27854 <02kazuki16@gmail.com>
2021-06-21 04:54:43 +0000

    [refactor]application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, only: [:show]
-	before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_up_path_for(resource)
-  	my_page_path
+    my_page_path
   end
 
   def after_sign_in_path_for(resource)
-  	my_page_path
+    my_page_path
   end
 
   def before_sign_in_path_for(resource)
-  	root_path
+    root_path
   end
 
   protected
