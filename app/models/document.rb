77a35e0a066281d41f411640915a89472601d511
KAZUKI27854 <02kazuki16@gmail.com>
2021-06-21 05:19:17 +0000

    [refactor]document.rb

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,10 +1,10 @@
 class Document < ApplicationRecord
-	belongs_to :user
-	belongs_to :goal
+  belongs_to :user
+  belongs_to :goal
 
-	validates :body, {presence: true, length: {maximum: 300}}
-	validates :milestone, {presence: true, length: {maximum: 100}}
-	validates :add_level, presence: true
+  validates :body, { presence: true, length: { maximum: 300 } }
+  validates :milestone, { presence: true, length: { maximum: 100 } }
+  validates :add_level, presence: true
 
-	attachment :document_image
+  attachment :document_image
 end
