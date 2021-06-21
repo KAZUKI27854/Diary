9cefbd75782bf6e5e776dee184d118337a75c66c
KAZUKI27854 <02kazuki16@gmail.com>
2021-06-21 05:54:50 +0000

    [refactor]rspec_test_02

--- spec/features/02_before_login_registration_spec.rb
+++ spec/features/02_before_login_registration_spec.rb
@@ -9,27 +9,27 @@ describe '2.ユーザログイン前の新規登録画面のテスト', type: :f
     it '「ぼうけんのはじまりじゃ」と表示される' do
       expect(page).to have_content 'ぼうけんのはじまりじゃ'
     end
-    
+
     it 'メッセージキャラが老師である' do
       expect(page).to have_css '.master'
     end
-    
+
     it 'なまえのフォームが表示される' do
       expect(page).to have_field 'user[name]'
     end
-    
+
     it 'メールフォームが表示される' do
       expect(page).to have_field 'user[email]'
     end
-    
+
     it 'パスワードフォームが表示される' do
       expect(page).to have_field 'user[password]'
     end
-    
+
     it 'かくにんようパスワードフォームが表示される' do
       expect(page).to have_field 'user[password_confirmation]'
     end
-    
+
     it 'とうろくボタンが表示される' do
       expect(page).to have_button 'とうろく'
     end
@@ -39,15 +39,15 @@ describe '2.ユーザログイン前の新規登録画面のテスト', type: :f
     it 'ログインリンクが表示される' do
       expect(page).to have_link 'ログイン'
     end
-    
+
     it 'GoogleのOAuth認証のリンクが表示される' do
       expect(page).to have_link 'GoogleOauth2でログインする'
     end
-    
+
     it 'FacebookのOAuth認証のリンクが表示される' do
       expect(page).to have_link 'Facebookでログインする'
     end
-    
+
     it 'TwitterのOAuth認証のリンクが表示される' do
       expect(page).to have_link 'Twitterでログインする'
     end
@@ -64,12 +64,12 @@ describe '2.ユーザログイン前の新規登録画面のテスト', type: :f
     it '正しく新規登録される' do
       expect { click_button 'とうろく' }.to change(User.all, :count).by(1)
     end
-    
+
     it '新規登録後のリダイレクト先がマイページである' do
       click_button 'とうろく'
       expect(current_path).to eq my_page_path
     end
-    
+
     it '新規登録時に「アカウントを作成しました」というフラッシュが表示される' do
       click_button 'とうろく'
       expect(page).to have_content 'アカウントを作成しました'
@@ -87,7 +87,7 @@ describe '2.ユーザログイン前の新規登録画面のテスト', type: :f
     it '新規登録に失敗している' do
       expect(User.all.count).to be 0
     end
-    
+
     it 'エラーメッセージが表示される' do
       click_button 'とうろく'
       expect(page).to have_selector '.error__message'
