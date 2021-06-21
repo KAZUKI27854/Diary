d162ac12b4c54c92951eb253893ceca3caa64d74
KAZUKI27854 <02kazuki16@gmail.com>
2021-06-21 04:47:40 +0000

    [refactor]TodoList/searches_controller.rb

--- app/controllers/todo_lists/searches_controller.rb
+++ app/controllers/todo_lists/searches_controller.rb
@@ -3,14 +3,14 @@ class TodoLists::SearchesController < ApplicationController
   before_action :set_current_user
 
   def index
-    if params[:category].blank? && params[:word].blank? then
+    if params[:category].blank? && params[:word].blank?
       todo_lists = @user.todo_lists
-    elsif params[:category].present? && params[:word].present? then
+    elsif params[:category].present? && params[:word].present?
 
-      lists_result_by_cat =  @user.goals.find_by(category: params[:category]).todo_lists
+      lists_result_by_cat = @user.goals.find_by(category: params[:category]).todo_lists
       todo_lists = lists_result_by_cat.where('body LIKE(?)', "%#{params[:word]}%")
 
-    elsif params[:category].present? && params[:word].blank? then
+    elsif params[:category].present? && params[:word].blank?
       todo_lists = @user.goals.find_by(category: params[:category]).todo_lists
     else
       todo_lists = @user.todo_lists.where('body LIKE(?)', "%#{params[:word]}%")
@@ -22,7 +22,7 @@ class TodoLists::SearchesController < ApplicationController
 
   private
 
-    def set_current_user
-      @user = current_user
-    end
+  def set_current_user
+    @user = current_user
+  end
 end
