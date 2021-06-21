91df4ac1df918a498b25641aa75bbf0a975fadae
KAZUKI27854 <02kazuki16@gmail.com>
2021-06-21 05:27:01 +0000

    [refactor]todo_list.rb

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -4,12 +4,12 @@ class TodoList < ApplicationRecord
   belongs_to :goal
   belongs_to :user
 
-  validates :body, {presence: true, length: {maximum: 100}}
+  validates :body, { presence: true, length: { maximum: 100 } }
   validate :day_after_today
 
-  #過去の日付登録を防ぐバリデーション（期限超過の場合、期限変更かチェックを入れることだけできる）
+  # 過去の日付登録を防ぐバリデーション（期限超過の場合、期限変更かチェックを入れることだけできる）
   def day_after_today
-    if deadline != nil && is_finished ==false
+    if !deadline.nil? && is_finished == false
       errors.add(:deadline, 'は、本日以降の日付を入力して下さい') if deadline.to_date < Date.today
     end
   end
