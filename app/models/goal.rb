167f096d60349be3798cf8a07527d09683c02241
KAZUKI27854 <02kazuki16@gmail.com>
2021-06-21 05:20:56 +0000

    [refactor]goal.rb

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -4,13 +4,13 @@ class Goal < ApplicationRecord
   has_many :documents, dependent: :destroy
   has_many :todo_lists, dependent: :destroy
 
-  validates :category, {presence: true, length: {maximum: 20}}
-  validates :goal_status, {presence: true, length: {maximum: 100}}
+  validates :category, { presence: true, length: { maximum: 20 } }
+  validates :goal_status, { presence: true, length: { maximum: 100 } }
   validates :deadline, presence: true
   validate :day_after_tomorrow
 
   def day_after_tomorrow
-    unless deadline == nil
+    unless deadline.nil?
       errors.add(:deadline, 'は、明日以降の日付を入力して下さい') if deadline.to_date <= Date.today
     end
   end
