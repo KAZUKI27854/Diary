d1af5ed4b13236f73a56f89b7aa3de170d06bf7c
KAZUKI27854 <02kazuki16@gmail.com>
2021-06-22 06:12:16 +0000

    [add]withdraw_validation to omniauth_callbacks_controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -15,7 +15,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def callback_for(provider)
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted?
+    if !@user.is_active
+      flash[:alert] = 'このアカウントは退会済みです。お手数ですが、別のアカウントをお使いください。'
+      redirect_to new_user_session_path
+    elsif @user.persisted?
       sign_in_and_redirect @user, event: :authentication
       flash[:notice] = "#{provider}アカウントでログインしました"
     else
