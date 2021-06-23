0ec165770284b664bf357c416c56f212de8c5d7c
KAZUKI27854 <02kazuki16@gmail.com>
2021-06-23 02:28:46 +0000

    [refactor]documents/searches_controller.rb

--- app/controllers/documents/searches_controller.rb
+++ app/controllers/documents/searches_controller.rb
@@ -4,16 +4,22 @@ class Documents::SearchesController < ApplicationController
 
   def index
     if params[:category].blank? && params[:word].blank?
+      
       documents = @user.documents
+      
     elsif params[:category].present? && params[:word].present?
 
       doc_result_by_cat = @user.goals.find_by(category: params[:category]).documents
       documents = doc_result_by_cat.where('body LIKE(?)', "%#{params[:word]}%")
 
     elsif params[:category].present? && params[:word].blank?
+      
       documents = @user.goals.find_by(category: params[:category]).documents
+      
     else
+      
       documents = @user.documents.where('body LIKE(?)', "%#{params[:word]}%")
+      
     end
 
     sort_documents = documents.includes(:goal).page(params[:page]).per(20).reverse_order
