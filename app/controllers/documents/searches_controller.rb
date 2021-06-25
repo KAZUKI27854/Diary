10c1d04336f6c758dbf7c438937761671103161b
KAZUKI27854 <02kazuki16@gmail.com>
2021-06-25 02:00:36 +0000

    [refactor]searches_controller

--- app/controllers/documents/searches_controller.rb
+++ app/controllers/documents/searches_controller.rb
@@ -4,24 +4,15 @@ class Documents::SearchesController < ApplicationController
 
   def index
     if params[:category].blank? && params[:word].blank?
-      
       documents = @user.documents
-      
     elsif params[:category].present? && params[:word].present?
-
-      doc_result_by_cat = @user.goals.find_by(category: params[:category]).documents
-      documents = doc_result_by_cat.where('body LIKE(?)', "%#{params[:word]}%")
-
+      doc_search_result_by_cat = @user.goals.find_by(category: params[:category]).documents
+      documents = doc_search_result_by_cat.where('body LIKE(?)', "%#{params[:word]}%")
     elsif params[:category].present? && params[:word].blank?
-      
       documents = @user.goals.find_by(category: params[:category]).documents
-      
     else
-      
       documents = @user.documents.where('body LIKE(?)', "%#{params[:word]}%")
-      
     end
-
     sort_documents = documents.includes(:goal).page(params[:page]).per(20).reverse_order
     render partial: "users/card", locals: { documents: sort_documents }
   end
