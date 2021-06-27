799ed7234e2f22ac7d9f45963148f6b17eab56c3
KAZUKI27854 <02kazuki16@gmail.com>
2021-06-27 00:34:25 +0000

    [update]schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -27,7 +27,7 @@ end
 require File.expand_path(File.dirname(__FILE__) + "/environment")
 set :environment, :production
 set :output, 'log/cron.log'
-every :day, at: jst('09:30 am') do
+every :day, at: jst('10:00 am') do
   begin
     runner "Batch::DataReset.data_reset"
     rake "db:seed:guest_user"
