9171b8b91a660b4d674fe0bea730c2e261894da7
KAZUKI27854 <02kazuki16@gmail.com>
2021-07-07 01:45:02 +0000

    [add]transaction to Documents/update

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -33,28 +33,34 @@ class DocumentsController < ApplicationController
     @document = Document.find(params[:id])
     goal = @document.goal
     update_goal = Goal.find(params[:document][:goal_id])
-    if @document.update(document_params)
-      # ドキュメント更新時に目標が変わっているかで条件分岐
-      case update_goal.id
-      # 目標が変わっていない場合、獲得レベルの差分だけ目標レベルに加える
-      when goal.id
-        origin_add_level = params[:document][:origin_add_level].to_i
-        update_add_level = params[:document][:add_level].to_i
-        goal.level += (update_add_level - origin_add_level)
-        goal.update(level: goal.level)
-      # 目標が変わっている場合、変更前の目標と変更後の目標のデータを更新
+    Document.transaction do
+      if @document.update(document_params)
+        # ドキュメント更新時に目標が変わっているかで条件分岐
+        case update_goal.id
+        # 目標が変わっていない場合、獲得レベルの差分だけ目標レベルに加える
+        when goal.id
+          origin_add_level = params[:document][:origin_add_level].to_i
+          update_add_level = params[:document][:add_level].to_i
+          goal.level += (update_add_level - origin_add_level)
+          goal.update(level: goal.level)
+        # 目標が変わっている場合、変更前の目標と変更後の目標のデータを更新
+        else
+          when_doc_create_goal_auto_update(update_goal.id)
+          when_doc_change_goal_origin_goal_auto_update
+        end
+        if params[:document][:document_image].present?
+          # S3への画像反映のタイムラグを考慮して3秒待機
+          sleep(3)
+        end
+        flash[:notice] = "きろくをへんこうしました"
+        redirect_to my_page_path
       else
-        when_doc_create_goal_auto_update(update_goal.id)
-        when_doc_change_goal_origin_goal_auto_update
+        render "edit"
       end
-      if params[:document][:document_image].present?
-        # S3への画像反映のタイムラグを考慮して3秒待機
-        sleep(3)
-      end
-      flash[:notice] = "きろくをへんこうしました"
-      redirect_to my_page_path
-    else
-      render "edit"
+      rescue => e
+        logger.error e.backtrace.join("\n")
+        flash[:alert] = "エラーが発生しました。管理者へお問い合わせください。"
+        redirect_to my_page_path
     end
   end
 
