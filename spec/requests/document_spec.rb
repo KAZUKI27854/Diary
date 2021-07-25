96981365f3ca8ac9801f1a8d30dc7c914f019ba0
KAZUKI27854 <02kazuki16@gmail.com>
2021-07-25 04:03:52 +0000

    [add]request_spec of Document/destroy

--- spec/requests/document_spec.rb
+++ spec/requests/document_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Documentsコントローラーのアクセス制御テスト', type: :request do
   let!(:goal) { create(:goal) }
   let!(:user) { User.first }
+  let!(:document) { create(:document, goal_id: goal.id) }
 
   describe 'createアクション' do
     before do
@@ -19,8 +20,6 @@ RSpec.describe 'Documentsコントローラーのアクセス制御テスト', t
   end
 
   describe 'editアクション' do
-    let!(:document) { create(:document, goal_id: goal.id) }
-
     context '未ログインの場合' do
       before do
         get edit_document_path(document.id)
@@ -53,8 +52,6 @@ RSpec.describe 'Documentsコントローラーのアクセス制御テスト', t
   end
   
   describe 'updateアクション' do
-    let!(:document) { create(:document, goal_id: goal.id) }
-
     context '未ログインの場合' do
       before do
         document_params = attributes_for(:document, goal: goal, body: "test")
@@ -77,6 +74,38 @@ RSpec.describe 'Documentsコントローラーのアクセス制御テスト', t
         login_as(user, :scope => :user)
         document_params = attributes_for(:document, goal: goal, body: "test")
         patch document_path(another_user_document.id), :params => { :document => document_params }
+      end
+      
+      it '302エラーが発生する' do
+        expect(response).to have_http_status "302"
+      end
+      it 'マイページにリダイレクトされる' do
+        expect(response).to redirect_to my_page_path
+      end
+    end
+  end
+  
+  describe 'destroyアクション' do
+    context '未ログインの場合' do
+      before do
+        delete "/documents/1"
+      end
+      
+      it '302エラーが発生する' do
+        expect(response).to have_http_status "302"
+      end
+      it 'ログイン画面にリダイレクトされる' do
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+    
+    context 'ログイン済みで他のユーザーの目標を削除しようとした場合' do
+      let!(:another_user_goal) { create(:goal) }
+      let!(:another_user_document) { create(:document, goal_id: another_user_goal.id) }
+
+      before do
+        login_as(user, :scope => :user)
+        delete "/documents/2"
       end
       
       it '302エラーが発生する' do
